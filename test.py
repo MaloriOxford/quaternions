43d64da4d5a534c41351d09d209104179228b0e7
MaloriOxford <abo7fg@virginia.edu>
2025-08-01 15:25:42 -0400

    Added 3D plotting

--- test.py
+++ test.py
@@ -1,29 +1,83 @@
 from quat import quat as q
 from dual_quat import dual_quat as dq
 import matplotlib.pyplot as plt
+from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d.proj3d import proj_transform
+from mpl_toolkits.mplot3d.axes3d import Axes3D
 import numpy as np
 
-q0 = q([0, 105, 5, 5])
-q1 = q([0.5, -0.5, 0.5, 0.5])
+class Arrow3D(FancyArrowPatch):
+
+    def __init__(self, x, y, z, dx, dy, dz, *args, **kwargs):
+        super().__init__((0, 0), (0, 0), *args, **kwargs)
+        self._xyz = (x, y, z)
+        self._dxdydz = (dx, dy, dz)
+
+    def draw(self, renderer):
+        x1, y1, z1 = self._xyz
+        dx, dy, dz = self._dxdydz
+        x2, y2, z2 = (x1 + dx, y1 + dy, z1 + dz)
+
+        xs, ys, zs = proj_transform((x1, x2), (y1, y2), (z1, z2), self.axes.M)
+        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
+        super().draw(renderer)
+        
+    def do_3d_projection(self, renderer=None):
+        x1, y1, z1 = self._xyz
+        dx, dy, dz = self._dxdydz
+        x2, y2, z2 = (x1 + dx, y1 + dy, z1 + dz)
+
+        xs, ys, zs = proj_transform((x1, x2), (y1, y2), (z1, z2), self.axes.M)
+        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
+
+        return np.min(zs) 
+
+def _arrow3D(ax, x, y, z, dx, dy, dz, *args, **kwargs):
+    '''Add an 3d arrow to an `Axes3D` instance.'''
+
+    arrow = Arrow3D(x, y, z, dx, dy, dz, *args, **kwargs)
+    ax.add_artist(arrow)
+
+
+setattr(Axes3D, 'arrow3D', _arrow3D)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+
+########################################################################################################
+
+q0 = q([0, 5, 5, 5])
+q1 = q([0.5, 0.5, 0.5, 0.5])
 dq0 = dq.from_trans(q0, q1)
 dq1 = dq(q([0.75, 0.25, 0, 0]), [5, 2, 3, 4])
 dq2 = dq.from_trans([0, 1, 2, 3], q([1, 2, 3, 4]).normalized())
 dq3 = dq0 * dq2
 
-print(dq3.as_trans()[0])
-# print(*dq3.as_trans())
 
-# vec = [1, 2, 3]
-# vec_rot = q1.rot_apply(vec)
+transforms = [dq0, dq0 * dq0, dq2, dq3]
+points = np.zeros((len(transforms), 3))
 
-# print(math.sqrt(vec[0] ** 2 + vec[1] ** 2 + vec[2] ** 2))
-# print(math.sqrt(vec_rot[0] ** 2 + vec_rot[1] ** 2 + vec_rot[2] ** 2))
+for idx, trans in enumerate(transforms) :
+    points[idx] = trans.as_trans()[0]
+    dir = trans.as_trans()[1].rot_apply([1, 0, 0])
 
-# print(vec, vec_rot)
+    ax.arrow3D(*trans.as_trans()[0],
+            *dir,
+            mutation_scale=10,
+            arrowstyle="-|>",
+            linestyle='dashed')
 
-points = np.array([dq0.as_trans()[0], dq2.as_trans()[0], dq3.as_trans()[0]])
+scatter = ax.scatter(points[:,0], points[:,1], points[:,2], c=range(np.shape(points)[0]), cmap='viridis')
 
-ax = plt.figure().add_subplot(projection='3d')
-ax.scatter(points[0], points[1], points[2])
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+
+ax.set_xlim(0, 10)
+ax.set_ylim(0, 10)
+ax.set_zlim(0, 10)
+
+fig.colorbar(scatter)
 
 plt.show()
